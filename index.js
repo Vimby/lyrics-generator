bc77b6d613fb2270c148a91f5fa391bdaea662cb
vimby <vimbysbuzz@gmail.com>
2024-09-30 10:49:08 +0200

    Added call to the API made other simple adjustments

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-
-function generateLyrics(event) {
-event.preventDefault();
-let lyricsElement = document.querySelector(".search-result");
-
-lyricsElement.innerHTML= "Hello World this is my song";
-
-console.log(lyricsElement);
-
+function generateResponse(response){
+    console.log(response.data);
+    let lyricsElement = document.querySelector(".search-result");
+    lyricsElement.innerHTML = response.data.answer;
+   
 }
 
 
+function generateLyrics(event) {
+event.preventDefault();
+let keyword = document.querySelector("#search-input");
+let apiKey = "0427ta248d3357oc689fb60cb0b7d838";
+let prompt = `Generate song  lyrics  titled  ${keyword.value}. `;
+let context =
+  "Assume you are a song writer that follows given instructions. The composed song lyrics must have 2 verses and a chorus.  The verses and chorus must be wrapped  in a <p></p> and the paragraphs must be separated by a <br/>. Remove astericks from the song lyrics results";
+ let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+ axios.get(url).then(generateResponse);
+
+ console.log("generating response.........")
+
+}
 
 
 
